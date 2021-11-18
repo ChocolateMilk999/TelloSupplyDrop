ff4591be15c802d921307758976e3448109ea81f
ChocolateMilk999 <92953891+ChocolateMilk999@users.noreply.github.com>
2021-11-18 13:39:24 -0800

    Create thecode.py

---
+++ thecode.py
@@ -0,0 +1,45 @@
+from djitellopy import Tello
+from time import sleep
+
+# Note: The drone will already start facing East
+
+tello = Tello()
+
+tello.connect()
+sleep(1)
+
+tello.move_up(183)
+sleep(1)
+
+tello.move_forward(153)
+sleep(1)
+
+tello.rotate_counter_clockwise(90)
+sleep(1)
+
+tello.move_forward(153)
+sleep(1)
+
+tello.rotate_clockwise(90)
+sleep(1)
+
+tello.move_down(106)
+sleep(1)
+
+tello.move_forward(91)
+sleep(1)
+
+tello.rotate_clockwise(90)
+sleep(1)
+
+tello.move_up(45)
+sleep(1)
+
+tello.move_forward(91)
+sleep(1)
+
+tello.rotate_counter_clockwise(90)
+
+tello.move_forward(183)
+
+tello.land()
